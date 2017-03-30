366eaf525d8d986d805772282af0c2a62730db0f
Andrea Silvestri <andrea@weborama.com>
2017-03-30 14:48:17 +0200

    Aggiungere i file di progetto.

---
+++ PoGo Notifier/www/scripts/platformOverrides.js
@@ -0,0 +1,4 @@
+﻿/*
+	Questo file viene sostituito con codice specifico della piattaforma presente nella cartella /merges.
+	Per altre informazioni, vedere http://taco.visualstudio.com/it-it/docs/configure-app/#Content.
+*/
